2cdfe8f31bdda9e9a7c8bfaa477da7192f977dc7
HecklerK <49901822+HecklerK@users.noreply.github.com>
2019-04-30 15:06:14 +0300

    Add files via upload

---
+++ ПР 17/ПР 17/KonusF.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ПР_17
+{
+    /// <summary>
+    /// Логика взаимодействия для KonusF.xaml
+    /// </summary>
+    public partial class KonusF : Window
+    {
+        public KonusF()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double R = Convert.ToDouble(TBR.Text);
+            double H = Convert.ToDouble(TBH.Text);
+            double So = System.Math.PI * R * R;
+            double Sb = System.Math.PI * R * (R * R + H * H);
+            SO.Text = "S осн = " + Convert.ToString(So);
+            SB.Text = "S бок = " + Convert.ToString(Sb);
+            SOB.Text = "S общ = " + Convert.ToString(So + Sb);
+            V.Text = "V = " + Convert.ToString(So + Sb + H);
+        }
+    }
+}
---
+++ ПР 17/ПР 17/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ПР_17
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            form.Close();
+        }
+
+        private void Sfera_Click(object sender, RoutedEventArgs e)
+        {
+            SferaF form2 = new SferaF();
+            form2.Show();
+            form.Hide();
+        }
+
+        private void Konus_Click(object sender, RoutedEventArgs e)
+        {
+            KonusF form2 = new KonusF();
+            form2.Show();
+            form.Hide();
+        }
+
+        private void UsechKon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Chelindr_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
